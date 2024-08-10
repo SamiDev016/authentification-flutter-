ee867bc69aa6c85e3b174f44e40edc7cf4cc792b
samidev016 <adelim0555@gmail.com>
2024-08-10 19:32:00 +0200

    finished login UI PAGE

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:authentification_design/components/my_button.dart';
 import 'package:authentification_design/components/my_text_field.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -7,6 +8,8 @@ class LoginPage extends StatelessWidget {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
+  void signUserIn() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,6 +17,7 @@ class LoginPage extends StatelessWidget {
       body: SafeArea(
         child: Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               const SizedBox(
                 height: 50,
@@ -26,7 +30,7 @@ class LoginPage extends StatelessWidget {
                 height: 50,
               ),
               Text(
-                "Welcom back",
+                "Hello There",
                 style: TextStyle(fontSize: 18, color: Colors.grey[700]),
               ),
               const SizedBox(
@@ -45,11 +49,9 @@ class LoginPage extends StatelessWidget {
                 isPassword: true,
                 controller: _passwordController,
               ),
-
               const SizedBox(
                 height: 15,
               ),
-
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 25),
                 child: Row(
@@ -63,12 +65,103 @@ class LoginPage extends StatelessWidget {
                   ],
                 ),
               ),
-
               const SizedBox(
                 height: 25,
               ),
+              MyButton(
+                text: "Sign in",
+                onTap: signUserIn,
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Divider(
+                      thickness: 0.5,
+                      color: Colors.grey[500],
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    "Or continue with",
+                    style: TextStyle(color: Colors.grey[500]),
+                  ),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  Expanded(
+                    child: Divider(
+                      thickness: 0.5,
+                      color: Colors.grey[500],
+                    ),
+                  ),
+                ],
+              ),
 
-              
+              const SizedBox(height: 15,),
+               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[100],
+                      borderRadius: BorderRadius.circular(8)
+                    ),
+                    padding: const EdgeInsets.all(5),
+                    child: const Column(
+                      children: [
+                        Icon(
+                          Icons.circle_notifications,
+                          size: 50,
+                        ),
+                        Text("Google")
+                      ],
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[100],
+                      borderRadius: BorderRadius.circular(8)
+                    ),
+                    padding: const EdgeInsets.all(5),
+                    child: const Column(
+                      children: [
+                        Icon(
+                          Icons.add_circle_outline,
+                          size: 50,
+                        ),
+                        Text("Facebook")
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+
+              const SizedBox(height: 30,),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Not a member?",
+                    style: TextStyle(
+                      color: Colors.grey[700]
+                    ),
+                  ),
+                  const SizedBox(width: 4,),
+                  const Text(
+                    "Register Now",
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ],
+              )
             ],
           ),
         ),
